839a10abc58ffc669317989818264e1cb87777cd
dagui <davaguir852@gmail.com>
2023-09-11 19:21:00 -0700

    completed jwt authentication

--- frontend/src/users/LoginForm.js
+++ frontend/src/users/LoginForm.js
@@ -27,8 +27,9 @@ function LoginForm() {
         
         const data = await response.json()
 
-        if (response.status == 200) {
+        if (response.status === 200) {
             setCurrentUser(data.user)
+            localStorage.setItem('token', data.token)
             history.push('/')
         } else {
             setErrorMessage(data.message)
